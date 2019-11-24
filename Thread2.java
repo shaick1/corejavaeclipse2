065667c7dfc2e299f38753750cf89a0d9572532c
shaickmohamed Sirajudeen <shaick.pop@gmail.com>
2019-11-24 17:03:31 +0530

    test

--- Thread2.java
+++ Thread2.java
@@ -1,7 +1,7 @@
 public class Thread2 implements Runnable{
 
     public void run() {
-        for (int i=0;i<10;i++) {
+        for (int i=0;i<5;i++) {
             System.out.println("Current i is "+i);
 
         }
